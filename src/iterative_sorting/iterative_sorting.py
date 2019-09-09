26456e48607e8aa3cc3fa4ae43662b61ff54e5ea
rmejia-tajc <rmejia.tajc@gmail.com>
2019-09-09 16:37:43 -0600

    Complete selection_sort function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,33 +1,43 @@
 # TO-DO: Complete the selection_sort() function below 
+
 def selection_sort( arr ):
-    # loop through n-1 elements
+
+    # Start with current index = 0
+    # For all indices EXCEPT the last index (loop through n-1 elements)
     for i in range(0, len(arr) - 1):
+
         cur_index = i
+
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
 
-        # while cur_index > 0 
+        # a. Loop through elements on right-hand-side of current index
+        for j in range(cur_index, len(arr)):
+
+            # and find the smallest element
+            if arr[cur_index] > arr[j]:
+
+                smallest_index = j
+
+                # b. Swap the element at current index with the smallest element found in above loop
+                arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
 
 
+        
+        # # a. Loop through elements on right-hand-side of current index
+        # for j in range(cur_index, len(arr)):
 
+        #     # and find the smallest element
+        #     if arr[j] < arr[smallest_index]:
+        #         smallest_index = j 
 
+        # temp = arr[smallest_index]
 
-        # Start with current index = 0
-
-        # For all indices EXCEPT the last index:
-
-        # a. Loop through elements on right-hand-side of current index and find the smallest element
-
-        # b. Swap the element at current index with the smallest element found in above loop
+        # # b. Swap the element at current index with the smallest element found in above loop
+        # arr[smallest_index] = arr[cur_index]
+        
+        # arr[cur_index] = temp
              
-
-
-
-        # TO-DO: swap
-
-
 
 
     return arr
@@ -35,6 +45,7 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
+
 def bubble_sort( arr ):
     
     # initial state
@@ -54,11 +65,24 @@ def bubble_sort( arr ):
                 # If elements in wrong position (relative to each other, swap them)
                 arr[i], arr[i+1] = arr[i+1], arr[i]
 
+                # If swap occurs, switch state for next pass
                 swapping = True
-                # If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+
+            # If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
 
     return arr
-    
+
+
+# def bubble_sort( arr ):
+#     # Loop through your array
+#     for i in range(len(arr)):
+#         for j in range(len(arr)):
+#             # Compare each element to its neighbor
+#             if arr[i] < arr[j]:
+#                 # If elements in wrong position (relative to each other, swap them)
+#                 arr[i], arr[j] = arr[j], arr[i]
+#     return arr
+
 
 
 # STRETCH: implement the Count Sort function below
